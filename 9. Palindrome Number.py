87999659166bbc7ecbc8163ca2fdacc2bee72051
ArifGS <arifgs901@gmail.com>
2026-02-06 07:21:03 +0800

    9. Palindrome Number (without convert to string)

--- 9. Palindrome Number.py
+++ 9. Palindrome Number.py
@@ -4,8 +4,18 @@ class Solution(object):
         :type x: int
         :rtype: bool
         """
-        mystr = str(x)[::-1]
-        if mystr == str(x):
-            return True
-        else:
+        # mystr = str(x)[::-1]
+        # if mystr == str(x):
+        #     return True
+        # else:
+        #     return False
+        if x < 0:
             return False
+        
+        copy = x
+        reverse = 0
+        
+        while copy > 0:
+            reverse = (reverse * 10) + (copy % 10)
+            copy //= 10
+        return reverse == x
