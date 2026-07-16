a23e3b66f6ab599c24723c25d5086126bce55724
ArifGS <arifgs901@gmail.com>
2026-07-17 04:25:19 +0800

    feat: complete function

--- Medium/39_Combination Sum.py
+++ Medium/39_Combination Sum.py
@@ -5,5 +5,19 @@ class Solution(object):
         :type target: int
         :rtype: List[List[int]]
         """
-        hasil = combination = []
+        hasil = []
         def backtrack(combination, target, index):
+            if target == 0:
+                hasil.append(combination)
+                return
+            if target < 0:
+                return
+            for i in range(index, len(candidates)):
+                backtrack(combination + [candidates[i]], target - candidates[i], i)
+        
+        backtrack([], target, 0)
+        return hasil
+    
+x = Solution()
+hasil = x.combinationSum([2,3],8)
+print(hasil)
