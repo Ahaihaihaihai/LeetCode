39a95cfc83a40273674a38d0eddb00cf89bfe5e2
ArifGS <arifgs901@gmail.com>
2026-07-21 01:13:14 +0800

    Style: Binary Operator

--- Easy/191_Number of 1 Bits.py
+++ Easy/191_Number of 1 Bits.py
@@ -4,16 +4,24 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        def int_to_bin(n):
-            biner = ''
-            while n > 0:
-                sisa = n % 2
-                biner = str(sisa) + biner
-                n = n // 2
-            return biner
+        ## String count method
+        # def int_to_bin(n):
+        #     biner = ''
+        #     while n > 0:
+        #         sisa = n % 2
+        #         biner = str(sisa) + biner
+        #         n = n // 2
+        #     return biner
         
-        biner = int_to_bin(n)
-        return biner.count('1')
+        # biner = int_to_bin(n)
+        # return biner.count('1')
+    
+        ## Binary Operator Method
+        weight = 0
+        while n:
+            weight += n&1
+            n = n >> 1
+        return weight
 
 x = Solution()
 print(x.hammingWeight(2147483645))
